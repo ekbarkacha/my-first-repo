6d64c42fdc8e3e4842729849fe235db280797cb3
ekbarkacha <emmanuel.barkacha@aims.ac.rw>
2025-03-18 11:49:53 +0000

    first commit

---
+++ calc.py
@@ -0,0 +1,4 @@
+def addition(a:int,b:int)->int:
+    return a+b
+def multiplication(a:int,b:int)->int:
+    return a*b
---
+++ main.py
@@ -0,0 +1,7 @@
+from calc import addition,multiplication
+
+if __name__ == "__main__":
+    a = 4
+    b = 5
+    print(f"The summation of {a} and {b} is {addition(a,b)}")
+    print(f"The multiplication of {a} and {b} is {multiplication(a,b)}")
